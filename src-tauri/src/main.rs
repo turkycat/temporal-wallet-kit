2a4452b3a4346a2a57141a72062f7c35df9d5476
Jesse Frush <jesse.frush@gmail.com>
2024-07-26 12:16:14 -0700

    remove isTestnet param on rust call, infer based on the receive descriptor

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,20 +2,17 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use bdk;
-use bdk::blockchain::Blockchain;
 use bdk::blockchain::ElectrumBlockchain;
 use bdk::electrum_client::Client;
-use bdk::{miniscript, KeychainKind, SyncOptions, Wallet};
-use std::sync::Mutex;
-
-static CLIENT: Mutex<Option<Client>> = Mutex::new(None);
+use bdk::SyncOptions;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn fetch_balance(receive: &str, change: &str, isTestnet: bool) -> String {
+fn fetch_balance(receive: &str, change: &str) -> String {
+    let is_testnet = receive.contains("tpub") || receive.contains("tprv");
     let receive: String = receive.to_string();
     let change: String = change.to_string();
-    let network: bdk::bitcoin::Network = match isTestnet {
+    let network: bdk::bitcoin::Network = match is_testnet {
         true => bdk::bitcoin::Network::Testnet,
         false => bdk::bitcoin::Network::Bitcoin,
     };
@@ -26,7 +23,7 @@ fn fetch_balance(receive: &str, change: &str, isTestnet: bool) -> String {
         bdk::database::MemoryDatabase::default(),
     )
     .unwrap();
-    let connection = match isTestnet {
+    let connection = match is_testnet {
         true => "electrum.blockstream.info:60001",
         false => "blockstream.info:110",
     };
@@ -35,29 +32,15 @@ fn fetch_balance(receive: &str, change: &str, isTestnet: bool) -> String {
     wallet.sync(&blockchain, SyncOptions::default()).unwrap();
     let balance = wallet.get_balance().unwrap();
     let transactions = wallet.list_transactions(false).unwrap();
-    // {
-    //     let client_guard = CLIENT.lock().unwrap();
-    //     if let Some(client) = &*client_guard {
-    //         let blockchain: ElectrumBlockchain = ElectrumBlockchain::from(client.);
-
-    //         wallet.sync(&client, SyncOptions::default()).unwrap();
-    //         client.do_something();
-    //     }
-    // }
     format!(
-        "This wallet has a confirmed balance of {} satoshis with {} transactions",
+        "This {} wallet has a confirmed balance of {} satoshis with {} transactions",
+        if is_testnet { "testnet" } else { "mainnet" },
         balance.confirmed,
         transactions.len()
     )
 }
 
 fn main() {
-    // {
-    //     let mut client_guard = CLIENT.lock().unwrap();
-    //     *client_guard = Some(Client::new("blockstream.info:110").unwrap());
-    // }
-
-    // client = Some(Client::new("blockstream.info:110").unwrap());
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![fetch_balance])
         .run(tauri::generate_context!())
--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,23 @@ function App() {
   const [balanceMessage, setBalanceMessage] = useState("");
   const [receive, setReceive] = useState("");
   const [change, setChange] = useState("");
-  const [isTestnet, setIsTestnet] = useState(false);
 
   async function fetchBalance() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setBalanceMessage(
-      await invoke("fetch_balance", { receive, change, isTestnet })
-    );
+    setBalanceMessage(await invoke("fetch_balance", { receive, change }));
   }
 
   return (
     <div className="container">
       <h1>Temporal Wallet Kit</h1>
       <p>
-        This is a simple React app that uses Tauri to interact with a Rust
-        backend. You can easily check balances with this app. It could be easily
-        extended to list transactions, and a simple sweep recovery feature that
-        could require users to type their mnemonics for simple empergency
-        recovery.
+        This is a native desktop application with a React frontend that uses
+        Tauri to interact with a Rust backend. It persists no data. You can
+        easily check balances with this app. It could be easily extended to list
+        transactions, and a simple sweep recovery feature that could require
+        users to type their mnemonics for simple empergency recovery,
+        potentially adding hardware device support. This took me about 2 hours
+        to implement.
       </p>
 
       <div className="row">
@@ -47,15 +46,6 @@ function App() {
           onChange={(e) => setChange(e.currentTarget.value)}
           placeholder="Change descriptor:"
         />
-        <div>
-          <input
-            type="checkbox"
-            id="testnet-checkbox"
-            checked={isTestnet}
-            onChange={(e) => setIsTestnet(e.currentTarget.checked)}
-          />
-          <label htmlFor="testnet-checkbox">is testnet</label>
-        </div>
         <button type="submit">Submit</button>
       </form>
 
